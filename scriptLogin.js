5b6cef1986a064decea73256c6cbf516b1a30206
Jera <jerameelperezm@gmail.com>
2023-09-28 18:56:00 -0500

    LOGIN CASI LISTO

---
+++ scriptLogin.js
@@ -0,0 +1,21 @@
+// Cambio de fondo basado en la hora del día
+function cambiarFondoSegunHora() {
+    const horaActual = new Date().getHours();
+    const login = document.querySelector('.login');
+    const fondo = document.getElementById('background');
+    const horaNocheInicio = 20; // Cambia esto a la hora en que consideras que comienza la noche
+    const horaNocheFin = 6;    // Cambia esto a la hora en que consideras que termina la noche
+
+    if (horaActual >= horaNocheInicio || horaActual < horaNocheFin) {
+        fondo.style.backgroundImage = "url('../img/fondomontaña4.jpeg')";
+        login.style.borderColor = "rgba(0, 0, 0, 0.4)";
+        login.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+    } else {
+        fondo.style.backgroundImage = "url('../img/fondomontaña2.jpeg')";
+        login.style.borderColor = "rgba(253, 197, 171, 0.4)";
+        login.style.backgroundColor = "rgba(71, 87, 131, 0.5)";
+    }
+}
+
+// Ejecuta la función al cargar la página
+window.addEventListener('load', cambiarFondoSegunHora);
