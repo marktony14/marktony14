30ee7c03244fbd1ff19b55071ca59f235b6de5e6
Jera <jerameelperezm@gmail.com>
2023-09-28 22:10:16 -0500

    img

--- scriptLogin.js
+++ scriptLogin.js
@@ -8,11 +8,11 @@ function cambiarFondoSegunHora() {
     const horaNocheFin = 6;    // Cambia esto a la hora en que consideras que termina la noche
 
     if (horaActual >= horaNocheInicio || horaActual < horaNocheFin) {
-        fondo.style.backgroundImage = "url('../img/fondomontana4.jpeg')";
+        fondo.style.backgroundImage = "url('img/fondomontana4.jpeg')";
         login.style.borderColor = "rgba(0, 0, 0, 0.2)";
         login.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
     } else {
-        fondo.style.backgroundImage = "url('../img/fondomontana2.jpeg')";
+        fondo.style.backgroundImage = "url('img/fondomontana2.jpeg')";
         login.style.borderColor = "rgba(253, 197, 171, 0.4)";
         login.style.backgroundColor = "rgba(71, 87, 131, 0.5)";
         nube.style.opacity = "0.5";
